38c0b70fe71ff4fc46571c91d3f4a9325e914842
Praise Lizzy <praisengeh@gmail.com>
2026-04-03 05:25:50 +0100

    fixed 403 forbidden

--- api/index.php
+++ api/index.php
@@ -8,7 +8,7 @@ if ($_SERVER["REQUEST_METHOD"] == "POST") {
     // Check if  password are correct
     if ($password === "1234") {
         $_SESSION["Username"] = $username;
-        header("Location: /api/welcome.php");
+        header("Location: welcome.php");
         exit();
     } else {
         $error = "oops! Incorrect password.";
@@ -34,7 +34,7 @@ if ($_SERVER["REQUEST_METHOD"] == "POST") {
         <input type="password" id="password" name="password" required><br><br>
         <button type="submit">Login</button>
     </form>
-    <a href="/api/welcome.php">Go to Welcome Page</a>
+    <a href="welcome.php">Go to Welcome Page</a>
     <?php if ($error): ?>
         <p style="color: red;"><?php echo $error; ?></p>
     <?php endif; ?>
